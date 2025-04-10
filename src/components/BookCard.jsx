70cf9e0e7d10aef67c91aa9cf07641699990707b
unknown <fatemehkalhor7911@gmail.com>
2025-04-10 17:59:26 +0430

    adding List of Favorits Books

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from 'react'
 import { FaHeart } from "react-icons/fa";
-import styles from "./BookCard.module.css"
+import styles from "./BookCard.module.css";
+import { useState } from "react";
 
-function BookCard({data:{title,author,image,language,pages}}) {
+function BookCard({ data, likedBooksHandler}) {
 
-    const[like,setLike]=useState(false)
+  const [like, setLike] = useState(false);
+  const { title, author, image, language, pages} = data;
 
-    const likeHandler=()=>{
-        setLike((like)=> !like)
-    }
+  const likeHandler = () => {
+    likedBooksHandler(data ,like)
+    setLike((like) => !like);
+  };
   return (
     <>
-        <div className={styles.card}>
-            <img src={image} alt={title} />
-            <div className={styles.info}>
-                <h3>{title}</h3>
-                <p>{author}</p>
-                <div>
-                    <span>{language}</span>
-                    <span>{pages}</span>
-                </div>
-            </div>
-            <button onClick={likeHandler}>
-                <FaHeart fontSize="1.5rem" color={like?"red" :"#e0e0e0"}/>
-            </button>
+      <div className={styles.card}>
+        <img src={image} alt={title} />
+        <div className={styles.info}>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <div>
+            <span>{language}</span>
+            <span>{pages}</span>
+          </div>
         </div>
+        <button onClick={likeHandler}>
+          <FaHeart fontSize="1.5rem" color={like ? "red" : "#e0e0e0"} />
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default BookCard
+export default BookCard;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import {books} from "../constants/mockData.js"
-import BookCard from './BookCard.jsx';
+import { useState } from "react";
+import { books } from "../constants/mockData.js";
+import BookCard from "./BookCard.jsx";
 function Books() {
+  const [liked, setLiked] = useState([]);
+
+  const likedBooksHandler = (book, status) => {
+    console.log(book, status);
+    if (status) {    //unlike a book 
+      const newLikedList = liked.filter((i) => i.id !== book.id);
+      setLiked(newLikedList);
+    }else{            //like a book
+      setLiked(liked=> [...liked , book])
+    }
+    console.log("liked:" , liked);
     
+  };
   return (
     <>
-        {books.map((book)=>(
-            <BookCard  key={book.id} data={book}/>
+      <div>
+        {books.map((book) => (
+          <BookCard
+            key={book.id}
+            data={book}
+            likedBooksHandler={likedBooksHandler}
+          />
         ))}
+      </div>
+      <div></div>
     </>
-  )
+  );
 }
 
-export default Books
+export default Books;
